84ff978658abb0723b89833d98d7b688502f2e46
Emmanuel Jolaiya <jolaiyaemmanuel@gmail.com>
2020-06-27 13:06:09 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+var h3= document.querySelector('h3');
+var color1=document.querySelector('.color1');
+var color2 = document.querySelector('.color2');
+var body=document.getElementById('gradient');
+var button = document.querySelector('button')
+
+function changeBackground(){
+
+   body.style.background= "linear-gradient(to right,"
+    + color1.value 
+    +"," 
+    + color2.value + ")";
+
+    h3.textContent=body.style.background + ';'
+
+}
+
+color1.addEventListener('input',changeBackground)
+
+color2.addEventListener('input',changeBackground)
+
+
+
+window.onload = function (){
+    changeBackground()
+}
+
+
+
+function randomNumber(){
+
+    var random1=(Math.floor(Math.random()*100))
+    var random2=(Math.floor(Math.random()*100))
+    var random3=(Math.floor(Math.random()*100))
+
+    var randomColor=body.style.background= `linear-gradient(to right,rgb(${random1},${random2},${random3}),rgb(${random1},${random2},${random3}))`
+    h3.textContent=randomColor + ';'
+}
+
+button.addEventListener('click',randomNumber)
